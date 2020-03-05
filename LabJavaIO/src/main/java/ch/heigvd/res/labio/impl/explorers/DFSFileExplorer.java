f3fe5b5946965b314243e39857c612db19dd6447
Florent Crisinel <florent.crisinel@gmail.com>
2020-03-05 21:12:19 +0100

    Fix typo in parameter name of DFSFileExplorer::explore

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -15,7 +15,7 @@ import java.io.File;
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
+  public void explore(File rootDirectory, IFileVisitor visitor) {
     throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
