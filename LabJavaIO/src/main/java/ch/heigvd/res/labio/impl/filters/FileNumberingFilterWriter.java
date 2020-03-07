036559ace23c750237fd3634a214cc0b05830fa7
Fl0Cri <florent@inetis.ch>
2020-03-07 17:16:07 +0100

    Refactor the write method for Strings in FileNumberingFilterWriter to use Utils.getNextLine() (it won't be used anywhere otherwise)

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,6 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -25,10 +26,44 @@ public class FileNumberingFilterWriter extends FilterWriter {
     super(out);
   }
 
+  /**
+   * Writes a portion of a string.
+   * Note: When using this method, each call will start an independent write. For example, when writing "\r" and
+   * then "\n", both will output a newline. This is different from the behavior of write(int c) that would write
+   * a single Windows-style newline.
+   *
+   * @param  str  String to be written
+   * @param  off  Offset from which to start reading characters
+   * @param  len  Number of characters to be written
+   *
+   * @throws  IndexOutOfBoundsException
+   *          If the values of the {@code off} and {@code len} parameters
+   *          cause the corresponding method of the underlying {@code Writer}
+   *          to throw an {@code IndexOutOfBoundsException}
+   *
+   * @throws  IOException  If an I/O error occurs
+   */
   @Override
   public void write(String str, int off, int len) throws IOException {
-    for (int i = off; i < off + len; i++) {
-      write(str.charAt(i));
+    ensureStreamStarted();
+
+    String[] parts = new String[] {
+            "",
+            str.substring(off, off + len)
+    };
+
+    while (!parts[1].isEmpty()) {
+      parts = Utils.getNextLine(parts[1]);
+
+      // If first element is empty, there is no newline and the result is stored in the second element
+      if (parts[0].isEmpty()) {
+        out.write(parts[1]);
+        break;
+      }
+
+      // If the first element is non-empty, it contains a single line and the second element contains the remaining text
+      out.write(parts[0]);
+      writeNewLine();
     }
   }
 
@@ -43,18 +78,14 @@ public class FileNumberingFilterWriter extends FilterWriter {
    * next char to see if it is a \n or not (in order to not break Windows newlines).
    * But this behavior has an impact on MacOS newlines (\r alone). Because we must wait for the next char, if the
    * last character output to the stream is a "\r" the next line number will not be output.
-   * For example, "L1\rL2\r" will output "1\tL1\r2\tL2\r"
-   *          but "L1\nL2\n" will output "1\tL1\n2\tL2\n3\t".
+   * For example, "#\r" will output "1\t#\r"
+   *          but "#\n" will output "1\t#\n2\t".
    *
    * @throws IOException If an I/O error occurs
    */
   @Override
   public void write(int c) throws IOException {
-    // Handle the first time something is output to the stream
-    if (currentLine == 0) {
-      currentLine++;
-      writeNewLine();
-    }
+    ensureStreamStarted();
 
     // Handle MacOS newlines
     if (lastChar == '\r' && c != '\n') {
@@ -70,6 +101,23 @@ public class FileNumberingFilterWriter extends FilterWriter {
     }
   }
 
+  /**
+   * Handle the first time something is output to the stream.
+   *
+   * @throws IOException If an I/O error occurs
+   */
+  private void ensureStreamStarted() throws IOException {
+    if (currentLine == 0) {
+      currentLine++;
+      writeNewLine();
+    }
+  }
+
+  /**
+   * Increment the line counter and write the number of the new line.
+   *
+   * @throws IOException If an I/O error occurs
+   */
   private void writeNewLine() throws IOException {
     out.write(Integer.toString(currentLine++));
     out.write('\t');
