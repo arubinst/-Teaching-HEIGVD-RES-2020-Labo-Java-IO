99af3fbdea68933c7a8004891f8cb110e5e06169
Fl0Cri <florent@inetis.ch>
2020-03-07 16:50:48 +0100

    Implement FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,6 +18,8 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int currentLine = 0;
+  private int lastChar;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,17 +27,52 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; i++) {
+      write(str.charAt(i));
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(String.copyValueOf(cbuf, off, len));
   }
 
+  /**
+   * Write a single character to the stream.
+   * Note: In order to handle Windows newlines (\r\n), if a \r is encountered it is mandatory to wait until the
+   * next char to see if it is a \n or not (in order to not break Windows newlines).
+   * But this behavior has an impact on MacOS newlines (\r alone). Because we must wait for the next char, if the
+   * last character output to the stream is a "\r" the next line number will not be output.
+   * For example, "L1\rL2\r" will output "1\tL1\r2\tL2\r"
+   *          but "L1\nL2\n" will output "1\tL1\n2\tL2\n3\t".
+   *
+   * @throws IOException If an I/O error occurs
+   */
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // Handle the first time something is output to the stream
+    if (currentLine == 0) {
+      currentLine++;
+      writeNewLine();
+    }
+
+    // Handle MacOS newlines
+    if (lastChar == '\r' && c != '\n') {
+      writeNewLine();
+    }
+
+    out.write(c);
+    lastChar = c;
+
+    // Handle UNIX and Windows newlines
+    if (c == '\n') {
+      writeNewLine();
+    }
+  }
+
+  private void writeNewLine() throws IOException {
+    out.write(Integer.toString(currentLine++));
+    out.write('\t');
   }
 
 }
