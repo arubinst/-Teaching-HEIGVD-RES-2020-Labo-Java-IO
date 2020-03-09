5923a4052fb1aced9f166e960bea67be0a2ae7c0
Haxos <a.mayocartes@protonmail.com>
2020-03-09 19:47:40 +0100

    fixing DFSFileExplorer

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -15,8 +15,26 @@ import java.io.File;
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    vistor.visit(rootDirectory);
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+    File[] children = rootDirectory.listFiles();
+    visitor.visit(rootDirectory);
+
+    // no children
+    if (children == null) {
+      return;
+    }
+
+    for (File child: children) {
+      if(child.isFile()) {
+        visitor.visit(child);
+      }
+    }
+
+    for (File child : children) {
+      if(child.isDirectory()) {
+        explore(child, visitor);
+      }
+    }
   }
 
 }
