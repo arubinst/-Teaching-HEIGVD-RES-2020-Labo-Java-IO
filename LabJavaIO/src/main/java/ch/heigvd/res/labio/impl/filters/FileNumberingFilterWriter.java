76309763caa70e5a3c44bf1abf70486837961664
frederic <frederiic@outlook.com>
2020-03-15 21:42:42 +0100

    FileNumberingFilterWriter Implemented

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -19,23 +19,46 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineNb = 1;
+  private boolean returnC = false;
+  private String res = Integer.toString(lineNb++) + '\t';
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len+off; i++) {
+      char d = cbuf[i];
+
+      if(returnC || d ==  '\n') {
+        if (returnC && d == '\n') {
+          res += "\r\n";
+        } else if (returnC) {
+          res += '\r';
+        } else if (d == '\n') {
+          res += '\n';
+        }
+        res += Integer.toString(lineNb++) + '\t';
+      }
+      if (d != '\n' && d != '\r'){
+        res += d;
+      }
+      returnC = d == '\r';
+    }
+    super.write(res, 0, res.length());
+    res = "";
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    char[] d = {(char)c};
+    write(d, 0, d.length);  }
 
 }
