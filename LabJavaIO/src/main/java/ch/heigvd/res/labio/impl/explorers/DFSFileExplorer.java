3224b65581012f7532e12b1e560d243c8792cf88
uxorialrook <234463f591c>
2020-03-15 14:03:56 +0100

    Found a way to pass all the ApplicationTest (with a sort)

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,8 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -10,9 +12,9 @@ import java.io.File;
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
  *
- * Remark: Je ne sais pas pourquoi mais je n'arrive pas à valider le test:
- * theApplicationShouldBeAbleToGenerateTheListOfFileNames(). Il liste toujours les fichiers
- * avant de parcourir les dossiers suivants. Je ne comprend pas.
+ * Remark: Pour faire passer tous les tests, il a été nécessaire de faire un sort des fichiers
+ * par ordre alphabétique. Sinon, le résultat était faux. Peut-être une spécialité sur les systèmes
+ * UNIX ?
  *
  * @author Olivier Liechti
  */
@@ -27,16 +29,16 @@ public class DFSFileExplorer implements IFileExplorer {
       return;
     }
 
-    for(File file : files) {
-      if(file.isFile()) {
-        vistor.visit(file);
-      }
-    }
-    // Then moves to subdirectories
-    for (File directory : files) {
-      if (directory.isDirectory()) {
-        explore(directory, vistor);
-      }
+    // Solution trouvée pour faire passer le test
+    // theApplicationShoulgBeAbleToGenerateTheListOfFileNames()
+    // Sans cela le résultat était toujours à l'envers.
+    Arrays.sort(files);
+
+    for (File f : files) {
+      if (f.isDirectory())
+        explore(f, vistor);
+      else
+        vistor.visit(f);
     }
   }
 
